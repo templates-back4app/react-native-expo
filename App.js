dc17efc5e34abbf88efa9c742b3d0796929a311b
nataliaconde <natalia.csouza@outlook.com>
2020-04-27 01:19:35 -0300

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Platform, StyleSheet, Text, View, AsyncStorage } from 'react-native';
+import keys from './constants/Keys';
+import Parse from "parse/react-native.js";
+
+Parse.setAsyncStorage(AsyncStorage);
+Parse.initialize(keys.applicationId, keys.javascriptKey);
+Parse.serverURL = keys.serverURL;
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  componentWillMount() {
+    this.createInstallation();
+  }
+
+  createInstallation = async () => {
+    const Installation = Parse.Object.extend(Parse.Installation);
+    const installation = new Installation();
+
+    installation.set("deviceType", Platform.OS);
+
+    await installation.save();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ constants/Keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    applicationId: '8ClAG1TppQuhYL4DCgGfckuWVWJetxvH26KJ8PF7',
+    javascriptKey: 'zbAEcqgbLLeGGFkfpHjmJM1OCfb5faMi98pbhUhh',
+    serverURL: 'https://parseapi.back4app.com/'
+}
